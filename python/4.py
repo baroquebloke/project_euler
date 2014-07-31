061c55744e3d77110806fb2d7037ebfb9da0f268
Jacob Farnsworth <Baroquebloke@Jacobs-MacBook.local>
2014-07-31 11:08:28 -0400

    optimized

--- python/4.py
+++ python/4.py
@@ -2,14 +2,17 @@
 # from the product of two 2-digit numbers is 9009 = 91 x 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
 # result: 906609
+# time: 0.433s
+#
+# *edit: optimized*
 
 def palind_num():
-    is_pal = None
-    for x in range(100, 1000):
-        for y in range(100, 1000):
+    is_pal = 0
+    for x in range(999, 99, -1):
+        for y in range(x, 99, -1):
             num = x * y
             if str(num) == str(num)[::-1]:
-                if is_pal is None or num > is_pal:
+                if is_pal < num:
                     is_pal = num
     return is_pal
 
---
+++ python/9.py
@@ -0,0 +1,2 @@
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
